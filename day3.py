c26c290bbaa032bc82364c993e4c0d45acca5b27
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-05 23:12:10 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -13,3 +13,28 @@ if(year%4==0):
    print("leap")
 else:
   print("not leap")
+  
+  # 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bill=0
+if(size=="S"):
+  bill=bill+15
+elif(size=="M"):
+  bill=bill+20
+else:
+  bill=bill+25
+if(add_pepperoni=="y"):
+  if(size=="S"):
+    bill=bill+2
+  else:
+    bill=bill+3
+if(extra_cheese=="y"):
+  bill=bill+1
+print(bill)
+
