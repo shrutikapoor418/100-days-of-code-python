4257950456be29a1d1fe6a0aeaafd8593d4b2685
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-08 00:02:15 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -2,3 +2,18 @@ print("hello")
 fruits=["apple","mango","litchi","raspberry","mulberry"]
 for fruit in fruits:
   print(fruit)
+  # 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+s=0
+c=0
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+  s=s+student_heights[n]
+  c=c+1
+av=int(s/c)
+print(av)
+
+#Write your code below this row 👇
+print(student_heights)
+print(sum(student_heights))
