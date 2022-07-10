106e6357609bf923a85f7276a052255446ee88f7
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-11 00:36:14 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -2,9 +2,38 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
-def greet():
+def greeot():
   print("ok")
   print("ol")
   print("lo")
 
+greeot()
+
+ #Simple Function
+def greet():
+  print("Hello shru")
+  print("How do you do shru?")
+  print("Isn't the weather nice today?")
 greet()
+
+#Function that allows for input
+#'name' is the parameter.
+#'Jack Bauer' is the argument.
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+greet_with_name("bhavya")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+#Calling greet_with() with Positional Arguments
+greet_with("shrutu", "Nowhere")
+#vs.
+greet_with("Nowhere", "Jack Bauershrutu")
+
+
+#Calling greet_with() with Keyword Arguments
+greet_with(location="London", name="Angela")
