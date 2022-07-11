24fe9d666d11ea5abe09d73805b72f87573afcef
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-11 08:10:33 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -70,3 +70,13 @@ def funcky(a="shruti",b="ayush",c=" "):
   print(a)
   print(b)
 funcky()  
+def prime_check():
+  is_prime=True
+  for(i in range(2,number):
+    if(number%i==0):
+      is_prime=False
+  if is_prime:
+      print("prime")
+  else:
+      print("not prime")
+      
