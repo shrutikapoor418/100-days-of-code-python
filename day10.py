963a814c192fc69222b36f98a25b0caaebbc858b
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-14 22:37:21 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -33,3 +33,43 @@ def format_name(f_name, l_name):
 
 
 
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year,month):
+ # month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  p=is_leap(year)
+  if p==True:
+    print(28)
+  if p==False and month%2==0:
+      print(30)
+  if p==False and month%2!=0:
+      print(31)
+
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
