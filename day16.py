ed73eb7045639f966aafaf714b6427c7fb7c3adf
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-20 21:13:18 +0530

    Update day16.py

--- day16.py
+++ day16.py
@@ -3,3 +3,25 @@ import another_module
 print(another_module.another_var)
 import turtle
 a=turtle.Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+another_module.py
+from turtle import Turtle
+tim=Turtle()
+print(tim)
+another_var=10
