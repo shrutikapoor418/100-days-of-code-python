05b2aab412ac5cda5e97338b30f422525d42b8f2
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-24 06:41:34 +0530

    Update day16.py

--- day16.py
+++ day16.py
@@ -32,3 +32,20 @@ myTable.add_row(["pavan putra", "1", "B", "95.0 %"])
 
 print(myTable)
 
+from prettytable import PrettyTable
+
+columns = ["Student Name", "Class", "Section", "Percentage"]
+
+myTabl = PrettyTable()
+
+# Add Columns
+myTabl.add_column(columns[0], ["Leanord", "Penny", "Howard",
+					"Bernadette", "Sheldon", "Raj", "Amy"])
+myTabl.add_column(columns[1], ["X", "X", "X", "X", "X", "X", "X"])
+myTabl.add_column(columns[2], ["B", "C", "A", "D", "A", "B", "B"])
+myTabl.add_column(columns[3], ["91.2 %", "63.5 %", "90.23 %", "92.7 %",
+										"98.2 %", "88.1 %", "95.0 %"])
+
+print(myTabl)
+
+
