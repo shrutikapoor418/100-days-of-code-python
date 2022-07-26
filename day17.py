3a9e0fd9f1d1003904f71d05a67035354840c6d5
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-07-26 12:32:20 +0530

    Update day17.py

--- day17.py
+++ day17.py
@@ -6,3 +6,36 @@ class Car:
 
 user1=Car("001","shruti")
 print(user1)
+
+
+
+#project quiz
+#main.py
+from question_model import Question
+from data import question_data
+question_bank=[]
+for question in question_data:
+    question_text=Question["text"]
+    question_answer = Question["answer"]
+    new_ques=Question(question_text, question_answer)
+    question_bank.append(new_ques)
+print(question_bank[0].answer)
+
+#data.py
+question_data = [
+{"text": "A slug's blood is green.", "answer": "True"},
+{"text": "The loudest animal is the African Elephant.", "answer": "False"},
+{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+{"text": "Google was originally called 'Backrub'.", "answer": "True"},
+{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+
+
