0e3ff4b6a92e8335f779a65440283b69778b10af
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-08-03 23:30:39 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -1,6 +1,16 @@
-def fun():
-  a=1+2
-  return a
+#draw box using turtle graphics
 
-p=fun()
-Print("githubbbbbbbbbbbbbbbbbbbbbbbb")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from turtle import *
+tim_tu = Turtle()
+tim_tu.shape("circle")
+tim_tu.color("green")
+tim_tu.forward(100)
+tim_tu.left(90)
+tim_tu.backward(100)
+tim_tu.right(90)
+tim_tu.backward(100)
+screen = Screen()
+tim_tu.left(90)
+tim_tu.forward(100)
+screen.exitonclick()
