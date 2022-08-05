2fe3e928ed2822d349d225d98a09c9b922c6cf20
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-08-05 08:46:02 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -26,3 +26,26 @@ for _ in range(15):
     tim.penup()
     tim.forward(10)
     tim.pendown()
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+no_of_sides=5
+for _ in range(no_of_sides):
+    angle=360/no_of_sides
+    tim.forward(100)
+    tim.right(angle)
+no_of_sides=4
+for _ in range(no_of_sides):
+   madangle=360/no_of_sides
+   tim.forward(100)
+   tim.right(madangle)
+
+no_of_sides=3
+for _ in range(no_of_sides):
+   madangles=360/no_of_sides
+   tim.forward(100)
+   tim.right(madangles)
