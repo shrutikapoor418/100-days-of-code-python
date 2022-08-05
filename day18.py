9efe63bb10fc2b0c4b76d6a5f2ca458544333459
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-08-05 09:00:03 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -49,3 +49,24 @@ for _ in range(no_of_sides):
    madangles=360/no_of_sides
    tim.forward(100)
    tim.right(madangles)
+
+
+#turtle with colors
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+########### Challenge 3 - Draw Shapes ########
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 20):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
