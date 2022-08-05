5229e04aa67e9729d75d507dde02ee61f25bba81
shruti kapoor <58940689+shrutikapoor418@users.noreply.github.com>
2022-08-05 10:48:13 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -86,3 +86,29 @@ for _ in range(200):
     tim.color(random.choice(colours))
     tim.forward(30)
     tim.setheading(random.choice(directions))
+
+ import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########### Challenge 5 - Spirograph ########
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
+
+
+
+
